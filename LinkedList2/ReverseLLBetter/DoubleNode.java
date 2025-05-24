1a08fba6ae1b4143a26dbf942b1d12f9bc1c761e
Shantanu Chaudhary <167026008+shantanu-chaudharyy@users.noreply.github.com>
2025-05-24 19:27:44 +0000

    Added Solution

---
+++ LinkedList2/ReverseLLBetter/DoubleNode.java
@@ -0,0 +1,8 @@
+package LinkedList2.ReverseLLBetter;
+
+public class DoubleNode<T> {
+
+        Node<Integer> head ;
+        Node<Integer> tail;
+  }
+
---
+++ LinkedList2/ReverseLLBetter/Node.java
@@ -0,0 +1,13 @@
+package LinkedList2.ReverseLLBetter;
+
+public class  Node<T> {
+
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
